1f8a5708a2e39e559ea5b6497d91bd8333a727ad
Noah Hilverling <noah@hilverling.com>
2018-05-22 12:10:53 +0000

    First test version

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+var count = {};
+count['a'] = 0;
+count['b'] = 0;
+count['c'] = 0;
+count['d'] = 0;
+
+io.on('connection', function(socket) {
+  for (var key in count) {
+      socket.emit('update_count', key + '_' + count[key]);
+  }
+
+  socket.on('click', function(msg){
+    count[msg]++;
+    console.log('Count for ' + msg + ': ' + count[msg]);
+    io.emit('update_count', msg + '_' + count[msg]);
   });
 });
 
